f2d6aa55a8676082e32c183a27b532a125032825
muqaddasazahra <muqaddasazahra@gmail.com>
2024-04-30 14:05:07 +0500

    Lifted state up and used props to pass state information to child components

--- 1-components-state-props/src/components/Board.jsx
+++ 1-components-state-props/src/components/Board.jsx
@@ -1,23 +1,33 @@
+import { useState } from "react";
 import Square from "./Square";
 
 export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  function handleSquareCLick(index) {
+    setSquares((prev) => {
+    const newArray=[...prev];
+    newArray[index]="X";
+    return newArray;
+
+    });
+  }
   return (
     <div className="h-screen w-full bg-purple-300 flex justify-center items-center">
       <div className="flex gap-1 flex-col border-white border-2">
         <div className="board-row flex gap-1">
-          <Square />
-          <Square />
-          <Square />
+          <Square value={squares[0]} onSquareClick={()=>handleSquareCLick(0)} />
+          <Square value={squares[1]} onSquareClick={()=>handleSquareCLick(1)} />
+          <Square value={squares[2]} onSquareClick={()=>handleSquareCLick(2)} />
         </div>
         <div className="board-row flex gap-1">
-          <Square />
-          <Square />
-          <Square />
+          <Square value={squares[3]} onSquareClick={()=>handleSquareCLick(3)}  />
+          <Square value={squares[4]} onSquareClick={()=>handleSquareCLick(4)}  />
+          <Square value={squares[5]}  onSquareClick={()=>handleSquareCLick(5)} />
         </div>
         <div className="board-row flex gap-1">
-          <Square />
-          <Square />
-          <Square />
+          <Square value={squares[6]} onSquareClick={()=>handleSquareCLick(6)}  />
+          <Square value={squares[7]} onSquareClick={()=>handleSquareCLick(7)}  />
+          <Square value={squares[8]} onSquareClick={()=>handleSquareCLick(8)}  />
         </div>
       </div>
     </div>
--- 1-components-state-props/src/components/Square.jsx
+++ 1-components-state-props/src/components/Square.jsx
@@ -1,14 +1,16 @@
-import { useState } from "react"
+import { useState } from "react";
 
-export default function Square() {
-    const [text,setText]=useState(null)
-    function handleClick()
-    {
-        setText('X')
-    }
+export default function Square({ value, onSquareClick }) //value props holds the state information passed down to Square component
+ {
+ /*const [text,setText]=useState(null)  {State is uplifted because the board component needs to 
+                                          know tha value of squares in order to decide winner}*/
+
   return (
-   <button onClick={handleClick} className="h-24 w-24 p-0 m-0 bg-purple-800 border-white border-2 block text-2xl text-white">
-    {text}
-   </button>
-  )
+    <button
+      onClick={onSquareClick}
+      className="h-24 w-24 p-0 m-0 bg-purple-800 border-white border-2 block text-2xl text-white"
+    >
+      {value}
+    </button>
+  );
 }
