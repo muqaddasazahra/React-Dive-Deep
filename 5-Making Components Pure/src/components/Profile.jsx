7527b0aa72cd3be85821933102cc895b6928ab6b
muqaddasazahra <muqaddasazahra@gmail.com>
2024-05-15 22:42:43 +0500

    Made component pure by using props for input

--- 5-Making Components Pure/src/components/Profile.jsx
+++ 5-Making Components Pure/src/components/Profile.jsx
@@ -1,28 +1,28 @@
 import Panel from './Panel.jsx';
 import { getImageUrl } from './utils.jsx';
 
-let currentPerson;
+
 
 export default function Profile({ person }) {
-  currentPerson = person;
+
   return (
     <Panel>
-      <Header />
-      <Avatar />
+      <Header person={person} />
+      <Avatar person={person} />
     </Panel>
   )
 }
 
-function Header() {
-  return <h1>{currentPerson.name}</h1>; 
+function Header({person}) {
+  return <h1>{person.name}</h1>; 
 }
 
-function Avatar() {
+function Avatar({person}) {
   return (
     <img
       className="avatar"
-      src={getImageUrl(currentPerson)}
-      alt={currentPerson.name}
+      src={getImageUrl(person)}
+      alt={person.name}
       width={50}
       height={50}
     />
