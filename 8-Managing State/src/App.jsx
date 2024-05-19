9f238052ec7e68dcf995c6356ec761677d19fa37
muqaddasazahra <muqaddasazahra@gmail.com>
2024-05-19 19:03:54 +0500

    Balls game-- isActive ball logic added

--- 8-Managing State/src/App.jsx
+++ 8-Managing State/src/App.jsx
@@ -15,17 +15,17 @@ function App() {
   return (
     <>
     <div className="h-screen w-full flex justify-center items-center">
-      <Ball color="bg-teal-500"   onClick={handleClick}   isActive={activeBtn===""} />
-      <Ball color="bg-amber-600"  onClick={handleClick}  isActive={activeBtn===""}/>
-      <Ball color="bg-purple-700"  onClick={handleClick}  isActive={activeBtn===""}/>
-      <Ball color="bg-purple-700"  onClick={handleClick}  isActive={activeBtn===""}/>
-      <Ball color="bg-amber-600"  onClick={handleClick}  isActive={activeBtn===""}/>
-      <Ball color="bg-teal-500"  onClick={handleClick}  isActive={activeBtn===""}/>
-      <Ball color="bg-purple-700"  onClick={handleClick}  isActive={activeBtn===""}/>
-      <Ball color="bg-teal-500"  onClick={handleClick}  isActive={activeBtn===""}/>
-      <Ball color="bg-amber-600"  onClick={handleClick}  isActive={activeBtn===""}/>
-      <Ball color="bg-amber-600"  onClick={handleClick}  isActive={activeBtn===""}/>
-      <Ball color="bg-purple-700"  onClick={handleClick}  isActive={activeBtn===""}/>
+      <Ball color="bg-teal-500"   onClick={handleClick}   isActive={activeBtn==="bg-teal-500"} />
+      <Ball color="bg-amber-600"  onClick={handleClick}  isActive={activeBtn==="bg-amber-600"}/>
+      <Ball color="bg-purple-700"  onClick={handleClick}  isActive={activeBtn==="bg-purple-700"}/>
+      <Ball color="bg-purple-700"  onClick={handleClick}  isActive={activeBtn==="bg-purple-700"}/>
+      <Ball color="bg-amber-600"  onClick={handleClick}  isActive={activeBtn==="bg-amber-600"}/>
+      <Ball color="bg-teal-500"  onClick={handleClick}  isActive={activeBtn==="bg-teal-500"}/>
+      <Ball color="bg-purple-700"  onClick={handleClick}  isActive={activeBtn==="bg-purple-700"}/>
+      <Ball color="bg-teal-500"  onClick={handleClick}  isActive={activeBtn==="bg-teal-500"}/>
+      <Ball color="bg-amber-600"  onClick={handleClick}  isActive={activeBtn==="bg-amber-600"}/>
+      <Ball color="bg-amber-600"  onClick={handleClick}  isActive={activeBtn==="bg-amber-600"}/>
+      <Ball color="bg-purple-700"  onClick={handleClick}  isActive={activeBtn==="bg-purple-700"}/>
     </div>
    
     </>
